6bc8a2b22859d5f22094e7bceeb133507fc91498
ekafitriyah112 <ekafitriyah002@gmail.com>
2023-10-21 13:05:09 +0700

    menjalankan project pertama di emulator

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import { View, Text } from "react-native"
+const App = () => {
+  return (
+    <View>
+      <Text> Selamat Datang</Text>
+    </View>
+  );
+}
+
+export default App;
